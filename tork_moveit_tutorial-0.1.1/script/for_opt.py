b6c5095d6bd0491a73c1d56f9d06990fb9486bb6
T.Hasegawa <t.hasegawa.853@ms.saitama-u.ac.jp>
2025-02-16 15:03:33 +0900

    軌道の変更

--- tork_moveit_tutorial-0.1.1/script/for_opt.py
+++ tork_moveit_tutorial-0.1.1/script/for_opt.py
@@ -32,7 +32,7 @@ theta_opt = [[ 0.0000,  0.0000,  0.0000,  0.0000,  0.0000,  0.0000,  0.0000,  0.
   -0.0983, -0.1377, -0.1833, -0.2342, -0.2896, -0.3482, -0.4089, -0.4701,
   -0.5308, -0.5894, -0.6448, -0.6957, -0.7413, -0.7807, -0.8133, -0.8389,
   -0.8577, -0.8699, -0.8766, -0.8790, -0.8790, -0.8790],
- [ 1.5708,  1.5708,  1.5708,  1.5669,  1.5559,  1.5358,  1.5051,  1.4630,
+ [ 1.5700,  1.5700,  1.5700,  1.5669,  1.5559,  1.5358,  1.5051,  1.4630,
    1.4095,  1.3449,  1.2702,  1.1866,  1.0958,  0.9996,  0.9002,  0.7996,
    0.7002,  0.6040,  0.5132,  0.4296,  0.3549,  0.2903,  0.2368,  0.1947,
    0.1640,  0.1439,  0.1329,  0.1290,  0.1290,  0.1290],
@@ -64,39 +64,31 @@ mapping['l_shoulder_p_joint'] = theta_opt[1]
 mapping['l_shoulder_r_joint'] = theta_opt[2]
 mapping['l_shoulder_y_joint'] = theta_opt[3]
 mapping['l_elbow_joint'] = theta_opt[4]
-# 'l_elbow_joint'の値をすべて2倍する
-mapping['l_elbow_joint'] = [2 * x for x in mapping['l_elbow_joint']]
+# 'l_elbow_joint'縺ｮ蛟､繧偵☆縺ｹ縺ｦ2蛟阪☆繧・mapping['l_elbow_joint'] = [2 * x for x in mapping['l_elbow_joint']]
 mapping['l_elbow_joint_dummy'] = theta_opt[5]
 mapping['l_wrist_y_joint'] = theta_opt[6]
-# 'l_wrist_y_joint'の値をすべて-1倍する
-mapping['l_wrist_y_joint'] = [-1 * x for x in mapping['l_wrist_y_joint']]
 mapping['l_wrist_r_joint'] = theta_opt[7]
 
 
 def get_trajectory():
     trajectory = RobotTrajectory()
     trajectory.joint_trajectory.joint_names = name_list
-    num_steps = len(theta_opt[0])  # 列数（= 時間ステップ数）
-    time_step = 0.5  # 各ステップの時間間隔（適宜調整）
-
+    num_steps = len(theta_opt[0])  # 蛻玲焚・・ 譎る俣繧ｹ繝・ャ繝玲焚・・    time_step = 0.5  # 蜷・せ繝・ャ繝励・譎る俣髢馴囈・磯←螳懆ｪｿ謨ｴ・・
     for i in range(num_steps):
         point = JointTrajectoryPoint()
 
-        # すべての関節について指令値を取得（なければ0）
-        joint_positions = []
+        # 縺吶∋縺ｦ縺ｮ髢｢遽縺ｫ縺､縺・※謖・ｻ､蛟､繧貞叙蠕暦ｼ医↑縺代ｌ縺ｰ0・・        joint_positions = []
         for joint in name_list:
             if joint in mapping:
-                joint_positions.append(mapping[joint][i])  # 指定された関節の値
+                joint_positions.append(mapping[joint][i])  # 謖・ｮ壹＆繧後◆髢｢遽縺ｮ蛟､
             else:
-                joint_positions.append(0.0)  # 指定がない関節は0
+                joint_positions.append(0.0)  # 謖・ｮ壹′縺ｪ縺・未遽縺ｯ0
 
-        # 取得した関節角度を設定
-        point.positions = joint_positions
+        # 蜿門ｾ励＠縺滄未遽隗貞ｺｦ繧定ｨｭ螳・        point.positions = joint_positions
 
-        # 時間を設定
-        point.time_from_start = rospy.Duration(time_step * (i + 1))
+        # 譎る俣繧定ｨｭ螳・        point.time_from_start = rospy.Duration(time_step * (i + 1))
 
-        # `trajectory` に追加
+        # `trajectory` 縺ｫ霑ｽ蜉
         trajectory.joint_trajectory.points.append(point)
 
     return trajectory
@@ -106,6 +98,13 @@ def main():
     group.set_max_velocity_scaling_factor(1.0)
     group.set_max_acceleration_scaling_factor(1.0)
 
+    # for initial position
+    for joint in name_list:
+        if joint in mapping:
+            rospy.loginfo(f"joint = {joint}")
+            group.set_joint_value_target(joint, mapping[joint][1])  # 謖・ｮ壹＆繧後◆髢｢遽縺ｮ蛟､    
+    group.go()
+    
     trajectory = get_trajectory()
     group.execute(trajectory, wait=True)
     group.go()
@@ -116,37 +115,3 @@ if __name__ == '__main__':
     init_node()
 
     main()
-    
-    # group = MoveGroupCommander(move_group_name)
-    
-    # # Pose Target 1
-    # rospy.loginfo( "Start Pose Target 1")
-    # pose_target_1 = Pose()
-    
-    # pose_target_1.position.x = 0.0
-    # pose_target_1.position.y = -0.6
-    # pose_target_1.position.z = 0.3
-    # pose_target_1.orientation.x = 1.0
-    # pose_target_1.orientation.y = 0.0
-    # pose_target_1.orientation.z = 0.0
-    # pose_target_1.orientation.w = 0.0
-    
-    # rospy.loginfo( "Set Target to Pose:\n{}".format( pose_target_1 ) )
-    # group.set_pose_target( pose_target_1 )
-    # group.go()
-    
-    # # Pose Target 2
-    # rospy.loginfo( "Start Pose Target 2")
-    # pose_target_2 = Pose()
-    
-    # pose_target_2.position.x = 0.6
-    # pose_target_2.position.y = 0.0
-    # pose_target_2.position.z = 0.3
-    # pose_target_2.orientation.x = -0.707
-    # pose_target_2.orientation.y = -0.707
-    
-    # rospy.loginfo( "Set Target to Pose:\n{}".format( pose_target_2 ) )
-    # group.set_pose_target( pose_target_2 )
-    # group.go()
-    
-    
