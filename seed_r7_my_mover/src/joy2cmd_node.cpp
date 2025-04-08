1ebe9d2c51a8fe0a8af8fed8aef788e489a9eb4d
hase111111 <hasehasehase61@gmail.com>
2025-04-08 20:12:29 +0900

    Fix: 減速しない問題の修正

--- seed_r7_my_mover/src/joy2cmd_node.cpp
+++ seed_r7_my_mover/src/joy2cmd_node.cpp
@@ -21,33 +21,43 @@ public:
     cmd_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 10);
 
     publish_timer_ = nh_.createTimer(ros::Duration(0.1), &Joy2Cmd::publishCmdVel, this);  // 10Hz
+
+    latest_cmd_.linear.x = latest_cmd_.linear.y = latest_cmd_.angular.z = 0.0;
   }
 
 private:
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
     std::lock_guard<std::mutex> lock(mtx_);
 
-    latest_cmd_.linear.x = scale_linear_ * joy->axes[linear_axis_x_];
-    latest_cmd_.linear.y = scale_linear_ * joy->axes[linear_axis_y_];
-    latest_cmd_.angular.z = scale_angular_ * joy->axes[angular_axis_];
+    axes_x = scale_linear_ * joy->axes[linear_axis_x_];
+    axes_y = scale_linear_ * joy->axes[linear_axis_y_];
+    axes_z = scale_angular_ * joy->axes[angular_axis_];
 
-    ROS_INFO("Publishing cmd_vel: linear = (%f, %f), angular.z = %f",
-      latest_cmd_.linear.x, 
-      latest_cmd_.linear.y, 
-      latest_cmd_.angular.z);
+    ROS_INFO("axes_x: %f, axes_y: %f, axes_z: %f", axes_x, axes_y, axes_z);
   }
 
   void publishCmdVel(const ros::TimerEvent&) {
     std::lock_guard<std::mutex> lock(mtx_);
 
+    updateCmdVel();
+
     cmd_pub_.publish(latest_cmd_);
+  }
+
+  void updateCmdVel() {
+    constexpr double rate = 0.1;
+
+    latest_cmd_.linear.x = axes_x * rate + latest_cmd_.linear.x * (1 - rate);
+    latest_cmd_.linear.y = axes_y * rate + latest_cmd_.linear.y * (1 - rate);
+    latest_cmd_.angular.z = axes_z * rate + latest_cmd_.angular.z * (1 - rate);
   }
 
   ros::NodeHandle nh_;
   ros::Subscriber joy_sub_;
   ros::Publisher cmd_pub_;
   ros::Timer publish_timer_;
-  int linear_axis_x_, linear_axis_y_, angular_axis_;
+  int linear_axis_x_, linear_axis_y_, angular_axis_;  // const
+  double axes_x{0.0}, axes_y{0.0}, axes_z{0.0};
   double scale_linear_, scale_angular_;
   geometry_msgs::Twist latest_cmd_;
   std::mutex mtx_;
