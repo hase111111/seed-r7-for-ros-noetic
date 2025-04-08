0b5331958da454863f7abf30e8a1b02fa9e5a64e
hase111111 <hasehasehase61@gmail.com>
2025-04-08 19:55:52 +0900

    Update: 出力の方法をタイマーに変更

--- seed_r7_my_mover/src/joy2cmd_node.cpp
+++ seed_r7_my_mover/src/joy2cmd_node.cpp
@@ -1,3 +1,6 @@
+
+#include <mutex>
+
 #include "ros/console.h"
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
@@ -8,34 +11,45 @@ public:
   Joy2Cmd() {
     // Load parameters if needed
     ros::NodeHandle private_nh("~");
-    private_nh.param("linear_axis_x", linear_axis_x_, 1);  // usually left stick vertical
-    private_nh.param("linear_axis_y", linear_axis_y_, 3);  // usually left stick vertical
-    private_nh.param("angular_axis", angular_axis_, 0);  // usually left stick horizontal
+    private_nh.param("linear_axis_x", linear_axis_x_, 1);
+    private_nh.param("linear_axis_y", linear_axis_y_, 3);
+    private_nh.param("angular_axis", angular_axis_, 0);
     private_nh.param("scale_linear", scale_linear_, 0.25);
     private_nh.param("scale_angular", scale_angular_, 0.25);
 
     joy_sub_ = nh_.subscribe("joy", 10, &Joy2Cmd::joyCallback, this);
     cmd_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 10);
+
+    publish_timer_ = nh_.createTimer(ros::Duration(0.1), &Joy2Cmd::publishCmdVel, this);  // 10Hz
   }
 
 private:
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
-    geometry_msgs::Twist cmd;
-    cmd.linear.x = scale_linear_ * joy->axes[linear_axis_x_];
-    cmd.linear.y = scale_linear_ * joy->axes[linear_axis_y_];
-    cmd.angular.z = scale_angular_ * joy->axes[angular_axis_];
-    cmd_pub_.publish(cmd);
+    std::lock_guard<std::mutex> lock(mtx_);
+
+    latest_cmd_.linear.x = scale_linear_ * joy->axes[linear_axis_x_];
+    latest_cmd_.linear.y = scale_linear_ * joy->axes[linear_axis_y_];
+    latest_cmd_.angular.z = scale_angular_ * joy->axes[angular_axis_];
+
     ROS_INFO("Publishing cmd_vel: linear = (%f, %f), angular.z = %f",
-      cmd.linear.x, 
-      cmd.linear.y, 
-      cmd.angular.z);
+      latest_cmd_.linear.x, 
+      latest_cmd_.linear.y, 
+      latest_cmd_.angular.z);
+  }
+
+  void publishCmdVel(const ros::TimerEvent&) {
+    std::lock_guard<std::mutex> lock(mtx_);
+    cmd_pub_.publish(latest_cmd_);
   }
 
   ros::NodeHandle nh_;
   ros::Subscriber joy_sub_;
   ros::Publisher cmd_pub_;
+  ros::Timer publish_timer_;
   int linear_axis_x_, linear_axis_y_, angular_axis_;
   double scale_linear_, scale_angular_;
+  geometry_msgs::Twist latest_cmd_;
+  std::mutex mtx_;
 };
 
 int main(int argc, char** argv) {
