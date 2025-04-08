4c568c20ba4bffaafbb1d17d1888fcc89f935ca3
hase111111 <hasehasehase61@gmail.com>
2025-04-08 19:41:21 +0900

    Update: 危険速度に達しないようパラメータを調整

--- seed_r7_my_mover/src/joy2cmd_node.cpp
+++ seed_r7_my_mover/src/joy2cmd_node.cpp
@@ -11,8 +11,8 @@ public:
     private_nh.param("linear_axis_x", linear_axis_x_, 1);  // usually left stick vertical
     private_nh.param("linear_axis_y", linear_axis_y_, 3);  // usually left stick vertical
     private_nh.param("angular_axis", angular_axis_, 0);  // usually left stick horizontal
-    private_nh.param("scale_linear", scale_linear_, 1.0);
-    private_nh.param("scale_angular", scale_angular_, 1.0);
+    private_nh.param("scale_linear", scale_linear_, 0.25);
+    private_nh.param("scale_angular", scale_angular_, 0.25);
 
     joy_sub_ = nh_.subscribe("joy", 10, &Joy2Cmd::joyCallback, this);
     cmd_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 10);
@@ -25,7 +25,10 @@ private:
     cmd.linear.y = scale_linear_ * joy->axes[linear_axis_y_];
     cmd.angular.z = scale_angular_ * joy->axes[angular_axis_];
     cmd_pub_.publish(cmd);
-    ROS_INFO("Publishing cmd_vel: linear.x = %f, angular.z = %f", cmd.linear.x, cmd.angular.z);
+    ROS_INFO("Publishing cmd_vel: linear = (%f, %f), angular.z = %f",
+      cmd.linear.x, 
+      cmd.linear.y, 
+      cmd.angular.z);
   }
 
   ros::NodeHandle nh_;
